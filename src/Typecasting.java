77b162c92ed7f11ebb49c3fe622520d8607bf1ff
unknown <disha@gmail.com>
2025-09-16 23:29:56 -0700

    added

---
+++ src/Typecasting.java
@@ -0,0 +1,22 @@
+package Corejava_tns;
+
+public class Typecasting {
+
+    public static void main(String[] args) {
+        int quatity=3;
+        double priceperitem=99.50;
+        double totalprice= quatity*priceperitem;
+
+        //narrowing
+
+        double discount=10.75;
+        int roundedDiscount =  (int)discount;
+
+        double finalAmount =totalprice-roundedDiscount;
+
+        System.out.println("Online Shopping summary");
+        System.out.println("final amount "+finalAmount);
+
+    }
+
+}
